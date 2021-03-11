1d3fafec95ed19fab31ddd72c845cf88a343b362
Thud Bloodseeker <antibody13@hotmail.com>
2021-03-11 18:57:18 +0700

    Add project files.

---
+++ ConsoleMongoDB/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+namespace ConsoleMongoDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoCRUD db = new MongoCRUD("AddressBook");                                    
+            db.InsertRecord("Users", new NameModel { FirstName = "TT", LastName = "AA" });
+
+            Console.WriteLine("done");
+        }
+
+    }
+
+    public class NameModel
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    public class MongoCRUD
+    {
+        IMongoDatabase db;
+
+        public MongoCRUD(string database)
+        {
+            var client = new MongoClient();
+            db = client.GetDatabase(database);          //connect which database exist ? create database
+        }
+
+       public void InsertRecord<T>(string table , T record)
+        {
+            var collection = db.GetCollection<T>(table);    //select which table exist ? create table
+            collection.InsertOne(record);                   //insert record into table
+        }
+    }
+}
